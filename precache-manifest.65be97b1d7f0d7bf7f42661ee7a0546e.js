da76d4b1f64aef1d8f17d744073e910032483cdc
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-04-28 08:40:49 +0200

    Updates

---
+++ precache-manifest.65be97b1d7f0d7bf7f42661ee7a0546e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fc81088c76a77f86232",
+    "url": "/weathernow/static/js/runtime~main.fd096881.js"
+  },
+  {
+    "revision": "a98fb4267b6d750a2abe",
+    "url": "/weathernow/static/js/main.2919341a.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/weathernow/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a98fb4267b6d750a2abe",
+    "url": "/weathernow/static/css/main.43d4edad.chunk.css"
+  },
+  {
+    "revision": "ec8292a4f694cb83471f72c4415eb465",
+    "url": "/weathernow/index.html"
+  }
+];
